602117fd9512e977ab36283c93ff7df425a9e5f8
charmingdarling <h1hasinai_f06@yahoo.com>
2023-10-09 23:16:58 -0700

    Added middleware for parsong JSON and urlencoded form data. Edited app.get for notes and returned response in json from database. Made an app.post edit to /notes, but unsure if this is correct. Help.

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ const app = express();
 // Specify on which port the Express.js server will run
 const PORT = 3002;
 
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Static middleware pointing to the public folder
 app.use(express.static("public"));
 
@@ -22,12 +26,19 @@ app.use(express.static("public"));
 app.get("/", (req, res) => res.send("Navigate to /notes.html"));
 
 // Create Express.js routes for '/notes' endpoint - Notes Route
-app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "public/notes.html"))
-);
+// Create a get request for all `/notes` that logs when a user visits that route
+// * Do I need to use /api/notes?
+
+app.get("/notes", (req, res) => {
+  console.info(`${req.method} request received to get notes`);
+  // Sending all notes to the client from db in json format of title and text.
+  return res.json(db);
+});
 
 // * I think I have to make a post request to the server to save the note to the db.json file ?
-// app.post("/notes",
+app.post("/notes", (req, res) => {
+  res.json(path.join(__dirname, "public/notes.html"));
+});
 
 //! Is this correct? Is /api/notes the correct endpoint or is it redundant to /notes?
 // Create Express.js routes for '/api/notes' endpoint - API Route
